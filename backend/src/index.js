c84918b1ab087ae880c53028d1a7fab347ff7a11
Khali <min427188@gmail.com>
2025-11-20 07:18:38 +0630

    clerk webhook setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,19 @@
 import express from 'express'
 import { ENV } from './config/env.js'
-import {clerkMiddleware}from'@clerk/express'
+import { clerkMiddleware } from '@clerk/express'
 import { connectDB } from './config/db.js'
-import userRouter from'./route/user.route.js'
-const app=express()
-const PORT=ENV.PORT
-app.use('/ping',(req,res)=>{
-    res.status(200).json({message:"your server is wake up"})
+import userRouter from './route/user.route.js'
+const app = express()
+const PORT = ENV.PORT
+app.use('/ping', (req, res) => {
+    res.status(200).json({ message: "your server is wake up" })
 })
+app.use(express.json())
+app.use('/api/user', userRouter)
 app.use(clerkMiddleware())
-app.use('/api/user',userRouter)
 
-app.listen(PORT,()=>{
+
+app.listen(PORT, () => {
     connectDB()
     console.log(`Your Server is Running on ${PORT}`)
 })
--- backend/src/route/user.route.js
+++ backend/src/route/user.route.js
@@ -1,8 +1,16 @@
 import express from'express'
 import { createUser } from '../controller/user.controller.js'
+import { handleClerkWebhook } from '../controller/webhook.controller.js'
 
 const router=express.Router()
 
-router.post('/',createUser)
+
+
+
+
+
+router.post('/webhooks/clerk', handleClerkWebhook)
+
+
 
 export default router
